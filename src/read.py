bb5ac47f3461dd5800bf8e790ab694bed0ccd4c2
staffeev <st.affeev@yandex.ru>
2024-02-28 19:03:52 +0300

    reading text from files

--- src/read.py
+++ src/read.py
@@ -1,4 +1,31 @@
 import pandas as pd
+import os
+import fitz 
+import docx
+
 
 def read(path="source"):
-    return pd.DataFrame()
+    texts = []
+    for filename in map(lambda x: path + "/" + x, os.listdir(path)):
+        file_extension = filename[filename.rindex(".") + 1:]
+        if file_extension == "pdf":
+            texts.append((filename, process_pdf_file(filename)))
+        elif file_extension in ("doc", "docx"):
+            texts.append((filename, process_docx_file(filename)))
+    return pd.DataFrame(texts, columns=["Filename", "Text"])
+
+
+def process_pdf_file(filename):
+    """Получает весь текст из pdf"""
+    doc = fitz.open(filename)
+    return "\n".join(["".join(page.get_text()) for page in doc])
+
+
+def process_docx_file(filename):
+    """Получает весь текст из docx"""
+    doc = docx.Document(filename)
+    return "\n".join([i.text for i in doc.paragraphs])
+
+
+if __name__ == "__main__":
+    print(read())
