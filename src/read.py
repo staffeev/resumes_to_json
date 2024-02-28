270df8326209250c000fbec5209aca135efe0fd3
koldun <eg.sharypov@yandex.ru>
2024-02-28 15:34:07 +0300

    Stemming

--- src/read.py
+++ src/read.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
 def read(path="source"):
-    return pd.DataFrame()
+    data = {"Text": ["I live in a house near the mountains. I have two brothers and one sister, and I was born last. My father teaches mathematics, and my mother is a nurse at a big hospital. My brothers are very smart and work hard in school. My sister is a nervous girl, but she is very kind. My grandmother also lives with us. She came from Italy when I was two years old. She has grown old, but she is still very strong. She cooks the best food!\
+My family is very important to me. We do lots of things together. My brothers and I like to go on long walks in the mountains. My sister likes to cook with my grandmother. On the weekends we all play board games together. We laugh and always have a good time. I love my family very much.", "Every year we go to Florida. We like to go to the beach.\
+My favorite beach is called Emerson Beach. It is very long, with soft sand and palm trees. It is very beautiful. I like to make sandcastles and watch the sailboats go by. Sometimes there are dolphins and whales in the water!\
+Every morning we look for shells in the sand. I found fifteen big shells last year. I put them in a special place in my room. This year I want to learn to surf. It is hard to surf, but so much fun! My sister is a good surfer. She says that she can teach me. I hope I can do it!"]}
+    df = pd.DataFrame(data=data)
+    return df
