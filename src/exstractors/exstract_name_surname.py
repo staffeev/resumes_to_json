e809712832d27374fa8ebc3324fda8a783797253
Vainslav <vladik_ww@mail.ru>
2024-02-28 20:41:51 +0300

    Added name exstraction

---
+++ src/exstractors/exstract_name_surname.py
@@ -0,0 +1,20 @@
+from nltk import ne_chunk, pos_tag, word_tokenize, download 
+from nltk.tree import Tree
+
+def exstract(text):
+    nltk_results = ne_chunk(pos_tag(word_tokenize(text)))
+    name_surname = []
+    for nltk_result in nltk_results:
+        if len(name_surname) < 2:
+            if type(nltk_result) == Tree:
+                name = ''
+                for nltk_result_leaf in nltk_result.leaves():
+                    name += nltk_result_leaf[0] + ' '
+                if nltk_result.label()=="PERSON":
+                    for word in name.split():
+                        print(word)
+                        name_surname.append(word)
+        else:
+            break
+    return name_surname
+
