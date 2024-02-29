06f57589eee791c5529576a530eaf8251d23fa73
Tronnert <dima.golovanov.2005@mail.ru>
2024-02-29 12:20:31 +0300

    geo2

--- src/extract_features.py
+++ src/extract_features.py
@@ -121,25 +121,11 @@ def extract_name_and_surname(text):
     return name_surname if name_surname else None
 
 
-def extract_location(x):
-    return locationtagger.find_locations(text=" ".join(x) if x else " ")
-
-
-def extract_country(location_obj):
-    return location_obj.countries[0] if location_obj.countries else None
-
-
-def extract_city(location_obj):
-    return location_obj.cities[0] if location_obj.cities else ""
-
-
-def extract_geo_information(df):
-    loc = df["Stemmed"].apply(extract_location)
-    countries = loc.apply(extract_country)
-    print("Field `Country` extracted")
-    cities = loc.apply(extract_city)
-    print("Field `City` extracted")
-    return countries, cities
+def extract_geo_information(text):
+    places = locationtagger.find_locations(text=text + " ")
+    cities = places.cities + [None]
+    countries = places.countries + places.other_countries + [None]
+    return cities[0], countries[0]
 
 
 def extract_features(df):
@@ -153,5 +139,6 @@ def extract_features(df):
     print("Fields `GitHub` and `LinkedIn` extracted")
     df["NameSurname"] = df["Text"].apply(extract_name_and_surname)
     print("Field `NameSurname` extracted")
-    df["Country"], df["City"] = extract_geo_information(df)
+    df["City"], df["Country"] = zip(*df["Text"].apply(extract_geo_information))
+    print("Fields `Country` and `City` extracted")
     return df
