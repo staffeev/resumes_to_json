bad01e0dfd71b33e6f5e9f6d114aeb15417c7d52
staffeev <st.affeev@yandex.ru>
2024-02-29 09:45:45 +0300

    union 2

--- src/extract_features.py
+++ src/extract_features.py
@@ -1,6 +1,7 @@
 from nltk import ne_chunk, pos_tag, word_tokenize, download 
 from nltk.tree import Tree
 import pandas as pd
+import locationtagger
 import re
 
 
@@ -42,11 +43,44 @@ def extract_name_an_surname(text):
                         name_surname.append(word)
         else:
             break
-    return name_surname
+    return name_surname if name_surname else None
+
+
+def extract_location(x):
+    return locationtagger.find_locations(text=" ".join(x) if x else " ")
+
+
+def extract_country(location_obj):
+    return location_obj.countries[0] if location_obj.countries else None
+
+
+def extract_city(location_obj):
+    return location_obj.cities[0] if location_obj.cities else ""
+
+
+def extract_geo_information(df):
+    loc = df["Stemmed"].apply(extract_location)
+    countries = loc.apply(extract_country)
+    print("Field `Country` extracted")
+    cities = loc.apply(extract_city)
+    print("Field `City` extracted")
+    return countries, cities
+
+
+# def extract_geo(df):
+#     df["Location"] = df["Stemmed"].apply(lambda x: locationtagger.find_locations(text=" ".join(x) if x else "a"))
+#     df["Country"] = df["Location"].apply(lambda x: x.countries[0] if x.countries else "")
+#     df["City"] = df["Location"].apply(lambda x: x.cities[0] if x.cities else "")
+#     df = df.drop(columns=["Location"])
+#     return df
 
 
 def extract_features(df):
     df["Email"] = df["Text"].apply(find_email)
+    print("Field `Email` extracted")
     df["Phone"] = df["Text"].apply(find_phone)
+    print("Field `Phone` extracted")
     df["NameSurname"] = df["Text"].apply(extract_name_an_surname)
+    print("Field `NameSurname` extracted")
+    df["Country"], df["City"] = extract_geo_information(df)
     return df
